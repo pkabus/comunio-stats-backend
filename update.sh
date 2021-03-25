3f8bd31909d5c6f6db6904a2a672102de2e5749b
pkabus <peter.kabus@gmx.de>
2021-03-25 14:49:58 +0100

    DEV-006 Add Dockerfile{.piprod} and update docker compose files

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PATH_REPO=/home/pi/projects/comunio-stats-backend
+
+git -C $PATH_REPO pull
+
+# if any files have been modified in the last 24 hours
+if [ "$(find $PATH_REPO/ -type f -mtime -1)" ]
+then
+    echo "Changes in directory $PATH_REPO detected, build docker image..."
+
+    # get project version
+    VERSION=$($PATH_REPO/mvnw -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+
+    # build pi production docker image
+    docker build -f $PATH_REPO/Dockerfile.piprod -t pi/comunio-stats-backend:$VERSION .
+
+    # stop running container with given name
+    # docker stop comunio-stats-backend
+
+    # run pi production docker container
+    # docker run -itd --rm --name comunio-stats-backend -p : pi/comunio-stats-backend:$VERSION
+fi
